53b6c3961c1475eee372528a10c26b5102550097
Skokus <skok2022@gmail.com>
2021-02-14 23:45:29 +0100

    Adding Coin.
    Wanted to check if Error will disappear

---
+++ src/sample/Coin.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Coin {
+    private int value;
+
+    public Coin(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+}
