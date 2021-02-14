ea8d1fd5926cc288074b619cc19d546890e81b34
Skokus <skok2022@gmail.com>
2021-02-15 00:35:09 +0100

    Added Coins

---
+++ src/sample/Coins.java
@@ -0,0 +1,22 @@
+package sample;
+
+import java.lang.reflect.Array;
+
+public class Coins {
+    private Coin[] coins;
+    private int[] counters;
+    public int limit; //one limit for all counters
+
+    public Coins(Coin[] coins){
+        this.coins = coins;
+        this.counters = new int[coins.length];
+        this.limit = 10;
+    }
+
+    public Coins(Coin[] coins, int[] counters, int limit){
+        this.coins = coins;
+        this.counters = counters;
+        this.limit = limit;
+    }
+
+}
